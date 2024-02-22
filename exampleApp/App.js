2a8da27060d45a2c0f59f0a78cb66e9859fb13fa
kateriley1 <95618509+kateriley1@users.noreply.github.com>
2024-02-22 15:41:52 +0000

    wip

--- exampleApp/App.js
+++ exampleApp/App.js
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Modal from "react-native-modal";
 
 export default function App() {
+  const [clickCount, updateClickCount] = useState(0);
+  const [isModalVisible, setIsModalVisible] = React.useState(false);
+  const buttonClicked = () => {
+    updateClickCount(clickCount + 1);
+  };
+  const resetClicked = () => {
+    updateClickCount(0);
+    setIsModalVisible(() => !isModalVisible)
+  };
+
+{/* <Modal isVisible={isModalVisible}>
+        <View style={{ flex: 1 }}>
+          <Text>Hello!</Text>
+          <Button title="Hide modal" onPress={handleModal} /> */}
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Click me!" onPress={buttonClicked} />
+      <Text>Button has been clicked {clickCount} times</Text>
+      <Button title="Reset" onPress={resetClicked} />
+      <Modal isVisible={isModalVisible}>
+        <View style={styles.container}>
+        {/* title="Count will be reset" */}
+        <Text>Count has been reset!</Text>
+          <Button title="Hide modal" onPress={resetClicked} />
+        </View>
+      </Modal>
       <StatusBar style="auto" />
     </View>
   );
